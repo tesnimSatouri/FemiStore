bca73a175c3a1fcdc89af74b61b16cd499a06c92
maryem001 <mariem.touzri@esprit.tn>
2025-04-05 19:51:37 +0100

    serveur de decouverte eurekea

--- src/main/java/com/esprit/microservice/gestion_commandes/GestionCommandesApplication.java
+++ src/main/java/com/esprit/microservice/gestion_commandes/GestionCommandesApplication.java
@@ -2,10 +2,10 @@ package com.esprit.microservice.gestion_commandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class GestionCommandesApplication {
 
 	public static void main(String[] args) {
