605c423ddb15c6bf6cda119b3fb3a049f19e9d1d
maryem001 <mariem.touzri@esprit.tn>
2025-04-05 18:27:25 +0100

    ajout de dependence de eureka

--- src/main/java/com/esprit/microservice/gestion_commandes/GestionCommandesApplication.java
+++ src/main/java/com/esprit/microservice/gestion_commandes/GestionCommandesApplication.java
@@ -2,8 +2,10 @@ package com.esprit.microservice.gestion_commandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class GestionCommandesApplication {
 
 	public static void main(String[] args) {
