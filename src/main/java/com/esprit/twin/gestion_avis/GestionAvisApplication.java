601802be8410950d9a0da7590c6822d8fbb3737b
walaammar <wala.ammar@esprit.tn>
2025-04-20 17:14:20 +0100

    last updates

--- src/main/java/com/esprit/twin/gestion_avis/GestionAvisApplication.java
+++ src/main/java/com/esprit/twin/gestion_avis/GestionAvisApplication.java
@@ -3,10 +3,12 @@ package com.esprit.twin.gestion_avis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 @CrossOrigin(origins = "http://localhost:4200") // Ajoutez cette ligne
 public class GestionAvisApplication {
 
